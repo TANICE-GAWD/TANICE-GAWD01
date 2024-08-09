09f3c51a69f46571617ac937496daa9a04df6dce
TANICE-GAWD <2006princesharma@gmail.com>
2024-08-09 12:47:25 +0530

    TECH POINT 01 EXPERIMENT

--- script.js
+++ script.js
@@ -1,48 +1,10 @@
-const { Button, TextField, Typography, Container, Box } = MaterialUI;
+function handleLogin() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
 
-function Login() {
-    const [username, setUsername] = React.useState('');
-    const [password, setPassword] = React.useState('');
-    const [error, setError] = React.useState('');
-
-    const handleLogin = () => {
-        if (username === '' || password === '') {
-            setError('Both fields are required!');
-        } else {
-            setError('');
-            alert(`Welcome, ${username}!`);
-        }
-    };
-
-    return (
-        <Container className="login-container">
-            <Typography variant="h4">Login</Typography>
-            <TextField
-                label="Username"
-                variant="outlined"
-                fullWidth
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                margin="normal"
-            />
-            <TextField
-                label="Password"
-                type="password"
-                variant="outlined"
-                fullWidth
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                margin="normal"
-            />
-            {error && <Typography color="error">{error}</Typography>}
-            <Box mt={2}>
-                <Button variant="contained" fullWidth onClick={handleLogin}>
-                    Login
-                </Button>
-            </Box>
-        </Container>
-    );
+    if (username === "" || password === "") {
+        alert("Both fields are required!");
+    } else {
+        alert(`Welcome, ${username}!`);
+    }
 }
-
-ReactDOM.render(<Login />, document.getElementById('root'));
-

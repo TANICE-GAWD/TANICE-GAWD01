84487f0445d38071a2ad17d24de67efe1149d91c
TANICE-GAWD <2006princesharma@gmail.com>
2024-08-10 11:44:17 +0530

    LOGIN PAGE TO  CONVERT TO REGISTRATION PAGE

--- script.js
+++ script.js
@@ -15,23 +15,3 @@ function togglePasswordVisibility() {
 function handleLogin() {
     // Handle login logic here
 }
-
-function removeEyeIcon() {
-    const passwordInput = document.getElementById("password");
-
-    // This removes any child elements that may be added to the password field
-    Array.from(passwordInput.parentElement.querySelectorAll('input[type="password"]::-ms-reveal, input[type="password"]::-webkit-clear-button')).forEach(el => {
-        el.style.display = 'none';
-    });
-
-    // General approach to remove any eye icon added by the browser
-    passwordInput.addEventListener('input', () => {
-        const nextSibling = passwordInput.nextElementSibling;
-        if (nextSibling && nextSibling.tagName === "BUTTON") {
-            nextSibling.remove();
-        }
-    });
-}
-
-// Call this function when the DOM content is loaded
-document.addEventListener('DOMContentLoaded', removeEyeIcon);
